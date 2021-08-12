79eafe459ae75aeab4c3604eb479937b80c47be8
mdavisprog <mdavisprog@gmail.com>
2021-08-12 09:15:02 -0700

    Basic test list for displaying chatters.
    This commit removes unneeded classes and styles and implements a basic chatters list mainly for styling purposes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Chatters(props) {
+  const Items = props.Items
+  const ItemsList = Items.map((Item) => <li>{Item}</li>)
+  return (
+    <div>
+      <ul className="Chatters">
+        {ItemsList}
+      </ul>
+    </div>
+  )
+}
+
 function App() {
+  const Items = ["One", "Two", "Three"]
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+      <h2>Chatters</h2>
+      <Chatters Items={Items}/>
     </div>
   );
 }
