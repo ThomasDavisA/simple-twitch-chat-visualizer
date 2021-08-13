75bc11796041840d4580d06b43b00a2e95f87df5
mdavisprog <mdavisprog@gmail.com>
2021-08-13 10:14:13 -0700

    Added Chatter component to the Chatters list.
    This component is made up of an Avatar component and a NamePlate component. Currently, we are just using the React logo for now sized down to 64px.

--- client/src/components/chatters/Chatters.js
+++ client/src/components/chatters/Chatters.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import './Chatters.css'
 
+function Avatar(props) {
+	return (
+		<img className="Avatar" src={window.location.origin + "/logo192.png"} alt="Avatar"></img>
+	)
+}
+
+function NamePlate(props) {
+	return (
+		<div className="NamePlate">
+			{props.name}
+		</div>
+	)
+}
+
+function Chatter(props) {
+	return (
+		<div className="Chatter">
+			<Avatar />
+			<NamePlate name={props.name} />
+		</div>
+	)
+}
+
 export default class Chatters extends React.Component {
 	constructor(props) {
 		super(props)
 		this.Items = props.Items
-		this.ItemsList = this.Items.map((Item) => <li>{Item}</li>)
+		this.ItemsList = this.Items.map((Item) => <li><Chatter name={Item}/></li>)
 	}
 
 	componentDidMount() {
