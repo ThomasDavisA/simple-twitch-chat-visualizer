f4199279b82b3de64d4f692ad8bef23333a0cd7f
mdavisprog <mdavisprog@gmail.com>
2021-08-22 10:13:51 -0700

    Avatar image split up into different parts with randomization.
    The avatar image has been split up into 3 different parts. Each part is an image and
    each image is rendered on top of each other to form a whole. Each part consists of
    a number of images that is randomly selected when the part is rendered.
    
    The root image is rendered at the normal position, while all other parts are rendered
    with their margin moved to the left by the size of the image.
    
    Each image currently is a 64x64 png. These images are loaded when the page is
    loaded and then a random element is retrieved when the avatar part is rendered.

--- client/src/components/avatar/Avatar.js
+++ client/src/components/avatar/Avatar.js
@@ -1,10 +1,33 @@
 import react from 'react'
 import './Avatar.css'
 
+function importAll(r) {
+	return r.keys().map(r);
+}
+
+const imgHeads = importAll(require.context('../../images/head'));
+const imgTorsos = importAll(require.context('../../images/torso'));
+const imgLegs = importAll(require.context('../../images/legs'));
+
+function getRandomElement(Elements) {
+	let Index = Math.floor(Math.random() * Elements.length);
+	return Elements[Index];
+}
+
+function Part(props) {
+	return (
+		<img className={props.className} src={getRandomElement(props.element).default} alt="Avatar" />
+	)
+}
+
 export default class Avatar extends react.Component {
 	render() {
 		return (
-			<img className="Avatar" src={window.location.origin + "/logo192.png"} alt="Avatar"></img>
+			<div>
+				<Part className="Avatar" element={imgHeads} />
+				<Part className="Avatar-Row-Child" element={imgTorsos} />
+				<Part className="Avatar-Row-Child" element={imgLegs} />
+			</div>
 		)
 	}
 }
