8465cf1492d43e94bec420c8e225bf78c1c6a32c
mdavisprog <mdavisprog@gmail.com>
2021-09-05 08:41:26 -0700

    Added environment variable to use a test background.
    The default behavior is to use a transparent background that allows for easy viewing
    when setting up a stream. This is difficult when testing so the environment variable
    REACT_APP_SOLID_BACKGROUND is added to change the App's css style to a test
    style with a solid background.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Chatters from './components/chatters/Chatters'
 
+const SOLID_BACKGROUND = process.env.REACT_APP_SOLID_BACKGROUND === "true";
+
+let AppClassName = SOLID_BACKGROUND ? "App-Test" : "App";
+
 function App() {
   return (
-    <div className="App">
+    <div className={AppClassName}>
       <Chatters />
     </div>
   );
