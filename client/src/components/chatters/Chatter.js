6a0cb4e6c59ee1e675220094434b97e3f836cf6a
mdavisprog <mdavisprog@gmail.com>
2021-09-12 17:09:46 -0700

    Implemented 'api/users/messages' endpoint for chatters.
    This commit implements calling the above api on the server. This api call retrieves the
    currently available list of messages that have been cached on the server and then
    notifies each Chatter component of any new messages they have broadcasted within
    the Twitch channel.
    
    The Chatter component has been updated to handle an array of messages and will
    continually update the array as it receives new messages from the server. The
    component itself will display each message in the list one by one on a 2 second
    timer. This behavior is a first pass implementation of displaying multiple chat
    messages.

--- client/src/components/chatters/Chatter.js
+++ client/src/components/chatters/Chatter.js
@@ -24,19 +24,59 @@ export default class Chatter extends React.Component {
 		this.state = {
 			key: props.key,
 			name: props.name,
-			message: props.message,
-			timeStamp: 0
+			messages: props.messages,
+			timeStamp: 0,
+			currentMessage: null,
+			timerID: null
 		};
+	}
+
+	componentDidMount() {
+		// The component could have been mounted with valid messages passed into the component.
+		// Just kick off a timer here to begin the normal message processing.
+		setTimeout(() => this.popMessage(), 100);
 	}
 
 	componentDidUpdate(prevProps, prevState, snapshot) {
 		if (this.state.key === prevProps.key) {
 			if (this.state.timeStamp < prevProps.timeStamp) {
+				let messages = this.state.messages.concat(prevProps.messages);
+				
+				let timerID = null;
+				if (this.state.timerID == null) {
+					// Set a small timeout to call the popMessage function to give the component time to
+					// propagate the updated data to the state.
+					timerID = setTimeout(() => this.popMessage(), 100);
+				}
+
 				this.setState({
-					message: prevProps.message,
-					timeStamp: prevProps.timeStamp
+					messages: messages,
+					timeStamp: prevProps.timeStamp,
+					timerID: timerID
 				});
 			}
+		}
+	}
+
+	popMessage() {
+		let messages = this.state.messages;
+
+		if (messages.length > 0) {
+			let item = messages.shift();
+
+			// Always set a timer. This gives the previous message time to render if it is the last message
+			// in the queue.
+			let timerID = setTimeout(() => this.popMessage(), 2000);
+
+			this.setState({
+				messages: messages,
+				currentMessage: item,
+				timerID: timerID
+			});
+		} else {
+			this.setState({
+				timerID: null
+			})
 		}
 	}
 
@@ -45,7 +85,7 @@ export default class Chatter extends React.Component {
 			<div className="Chatter">
 				<NamePlate name={this.state.name} />
 				<Avatar id={this.state.name} />
-				{this.state.message && <ChatBubble key={this.state.timeStamp} message={this.state.message} />}
+				{this.state.currentMessage && <ChatBubble key={this.state.currentMessage.timeStamp} message={this.state.currentMessage.message} />}
 			</div>
 		)
 	}
