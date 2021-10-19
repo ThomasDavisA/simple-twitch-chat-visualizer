d204c452be100c6f613cd983dbdc6c72b896ef79
ThomasDavisA <ThomasDavisA@gmail.com>
2021-10-19 07:09:01 -0700

    added test data, created game loop

--- pixi-client/public/files/pixi-renderer.js
+++ pixi-client/public/files/pixi-renderer.js
@@ -9,6 +9,13 @@ fetcher((data) => {
 	});
 });
 
+const TextureCache = PIXI.utils.TextureCache,
+    Loader = PIXI.Loader.shared,
+    Resources = PIXI.Loader.shared.resources,
+    Sprite = PIXI.Sprite;
+
+let kobolds = [];
+
 const app = new PIXI.Application({ width: 360, height: 480 });
 document.body.appendChild(app.view);
 
@@ -17,14 +24,58 @@ app.renderer.view.style.display = 'block';
 app.renderer.autoDensity = true;
 app.resizeTo = window;
 
-const koboldContainer = new PIXI.Container();
+const koboldTexture = TextureCache['files/sprites/kobold/Kobold_001.png'];
 
-app.stage.addChild(koboldContainer);
+Loader.add('files/sprites/kobold/Kobold_001.png')
+    .load(setup);
 
-const koboldTexture = PIXI.Texture.from('files/sprites/kobold/Kobold_001.png');
+function setup() {
+    const koboldList = [
+        {
+            name: 'test1',
+            posx: 100,
+            posy: 100
+        },
+        {
+            name: 'test2',
+            posx: 50,
+            posy: 100
+        },
+        {
+            name: 'test3',
+            posx: 25,
+            posy: 100
+        }
+    ]
 
-const kobold = new PIXI.Sprite(koboldTexture);
-koboldContainer.addChild(kobold);
+    koboldList.forEach(kobold => {
+        const koboldSprite = new Sprite(Resources['files/sprites/kobold/Kobold_001.png'].texture);
+        koboldSprite.x = kobold.posx;
+        koboldSprite.y = kobold.posy;
+        koboldSprite.scale.x = .25;
+        koboldSprite.scale.y = .25;
+        kobold.wanderTick = 0;
+        kobold.koboldSprite = koboldSprite;
 
-koboldContainer.x = app.screen.width / 2;
-koboldContainer.y = app.screen.height / 2;
+        kobolds.push(kobold);
+    
+        app.stage.addChild(koboldSprite);
+        console.log('app stage added for ', kobold.name);
+    })
+
+    console.log(kobolds)
+    app.ticker.add((delta) => gameLoop(delta));
+}
+
+function gameLoop(delta) {
+    kobolds.forEach(kobold => {
+        const { wanderTick, koboldSprite } = kobold;
+        kobold.wanderTick--;
+        if (wanderTick < 0) {
+            koboldSprite.x = Math.floor(Math.random() * 300);
+            kobold.wanderTick = 60;
+        }
+    })
+
+    //console.log('gameloop ticked');
+}
